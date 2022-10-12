8bc3e18053b0daec08bce6bcd6a7184afb9ef302
ashli-ann petrekin <ashpetrekin@gmail.com>
2022-10-12 00:01:19 -0500

    Added files

---
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -0,0 +1,63 @@
+window.addEventListener("load", (event)=>{
+
+    let squares  = document.querySelectorAll("#board > div");
+    let newbt =  document.querySelector(".btn");
+
+    squares.forEach((sq)=> {
+        sq.classList.add("square");
+        sq.innerHTML = " ";
+    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
