e2b0b767e2f0d0b95ab63a5cd358049d649744cf
ashli-ann petrekin <ashpetrekin@gmail.com>
2022-10-13 05:21:11 -0500

    added click function

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,63 +1,51 @@
-window.addEventListener("load", (event)=>{
 
-    let squares  = document.querySelectorAll("#board > div");
-    let newbt =  document.querySelector(".btn");
-
-    squares.forEach((sq)=> {
-        sq.classList.add("square");
-        sq.innerHTML = " ";
+     
+window.onload = function() {
+        const squares  = document.querySelectorAll("#board > div");
+        const newbt =  document.querySelector(".btn");
+        // let gameactive = false;
+        // let player=X;
     
-    });
+        
+        
+        squares.forEach((sq)=> {
+            sq.classList.add("square");
+            sq.innerHTML = " ";
+        });
+
+        squares.forEach((sq)=> {
+           sq.addEventListener("click",function(){
+            sq.innerHTML=player;
+            sq.classList.add(player);
+
+            if( player == "X" ){
+                player= "O"
+                sq.classList.add(player);
+                sq.innerHTML = player;
+            }
+            else if (player == "O")
+            {
+                player= "O" 
+                sq.classList.add(player);
+                sq.innerHTML = player;
+            }
+
+
+           })
+        });
+
+        //changes the color of the squares when hovered over 
+        squares.forEach(function(elem,index){
+            elem.addEventListener("mouseover", function(e){
+                e.target.classList.add("hover");
+            });
+            elem.addEventListener("mouseout", function(e){
+                e.target.classList.remove("hover");
+            });
+
+        });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+}
